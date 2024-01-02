8caeddc3aecb0d03cd551520d4597c0237b8ad53
Zeben <zebenzuilopezconde@gmail.com>
2024-01-02 10:31:53 +0000

    first commit Zeben

--- app/src/main/java/com/example/actividadevaluativaspotify/MainActivity.kt
+++ app/src/main/java/com/example/actividadevaluativaspotify/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ActividadEvaluativaSpotifyTheme {
                 Surface{
+                    Text(text = "Hello World!")
                 }
             }
         }
